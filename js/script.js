500e4d1dc8e5262512aafc87108a86f0c44bc661
AHMADQURAAN97 <ahmadqouraan@gmail.com>
2021-06-22 21:59:38 +0300

    add prompts

--- js/script.js
+++ js/script.js
@@ -5,6 +5,7 @@ alert("WELCOME TO MY PAGE" +" "+ names  );
 
 let x = prompt("Do you know me (yes or no)?");
 x.toLowerCase(); console.log(x);
+ 
 if (x == "yes"||x=="y") {
     let y = prompt("Where and How ?");
     console.log(y)
@@ -79,3 +80,48 @@ switch(people.toLowerCase()) {
                     alert("Try to think about your decision")
                     break;
 }
+
+let h=prompt("What is your rate about this page?")
+
+for(let i=0 ;i<=3 ; i++ ) {
+        if(h==50){
+            alert (" you got the correct answer ")
+            break;
+        }
+         else if (h>50){
+            alert ("too high")
+            h=prompt("What is your rate about this page?");
+        }else if (h<50) {
+            alert ("too low")
+            h=prompt("What is your rate about this page?");
+        }
+if(i==3){
+    alert('the correct answer is 50')
+}
+    }
+
+
+
+
+    let remo=['Introduction','My Goals!','top ten places', 'facebook', 'twitter']
+    let mark = prompt("What most of thing you like it in my webpage" + remo + " ?")
+
+ for(let rt=o ; rt<=5 ; i++ ){
+ if (mark == "Introduction" || mark == "My Goals" || mark == "top ten places"){
+     mark=prompt("correct answer" , remo)
+    alert("It's Nice")
+     break;
+ }
+ else if (mark == "facebook" || mark == "twitter"){
+     alert("it's wrong")
+     mark=prompt("What most of thing you like it in my webpage ?")
+ }
+
+
+ if (rt==5){
+     alert('Your attempts are over');
+     
+ }
+}
+
+let grade=prompt("")
