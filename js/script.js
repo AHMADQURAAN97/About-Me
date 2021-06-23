c9a22cc9df87cc6bca89f5642cbff6a23e48a2f1
AHMADQURAAN97 <ahmadqouraan@gmail.com>
2021-06-23 14:44:35 +0300

    add prommpt and alert

--- js/script.js
+++ js/script.js
@@ -1,127 +1,130 @@
-'use strict';
+"use strict";
 
-let names=prompt("What's Your Name ?")
-alert("WELCOME TO MY PAGE" +" "+ names  );
+let names = prompt("What's Your Name ?");
+alert("WELCOME TO MY PAGE" + " " + names);
+
+let score = 0;
+let grade = function () {
+  score++;
+};
 
 let x = prompt("Do you know me (yes or no)?");
-x.toLowerCase(); console.log(x);
- 
-if (x == "yes"||x=="y") {
-    let y = prompt("Where and How ?");
-    console.log(y)
-} else if (x == "no"||x=="n") {
-    // console.log(x)
-    alert("You will become Know me");
+x.toLowerCase();
+console.log(x);
+
+if (x == "yes" || x == "y") {
+  let y = prompt("Where and How ?");
+  console.log(y);
+  grade();
+} else if (x == "no" || x == "n") {
+  // console.log(x)
+  alert("You will become Know me");
 } else {
-    console.log(x);
+  console.log(x);
 }
 
-let userage = prompt("Your age more than 18 (yes or no)? ")
+let userage = prompt("Your age more than 18 (yes or no)? ");
 switch (userage.toLowerCase()) {
-    case 'yes':
-    case 'y':
-        // console.log(userage);
-        alert("Welcome to our page");
-        break;
+  case "yes":
+  case "y":
+    // console.log(userage);
+    alert("Welcome to our page");
+    grade();
+    break;
 
-    case 'no':
-    case 'n':
-        // console.log(userage);
-        alert("Sorry you can/'t enter to this page");
-                 break;
+  case "no":
+  case "n":
+    // console.log(userage);
+    alert("Sorry you can/'t enter to this page");
+    break;
 
-                 default:
-                     alert("should write yes or no");
+  default:
+    alert("should write yes or no");
 }
 
- let details = prompt("Interested in know more details about Ahmad (yes or no)?");
- x.toLowerCase(); console.log(x);
- if (details=="yes"||"y"){
-    // console.log(details);
-     alert("Thank You")
- } else if (details=="no"||"n") {
-    //  console.log(details);
-     alert("Not Problem")
-
- }
-
-
-let people= prompt ("Are you working or is still student(YES OR NO)?")
-
-switch(people.toUpperCase()) {
-    case 'YES':
-    case 'Y':
-        alert("GOOD LUCK");
-        // console.log(people);
-        break;
-
-        case 'NO':
-            case 'N':
-                alert("best wishes for you")
-                // console.log(people);
-                break;
-
-                defult :
-                alert ("wrong answer")
+let details = prompt(
+  "Interested in know more details about Ahmad (yes or no)?"
+);
+x.toLowerCase();
+console.log(x);
+if (details == "yes" || "y") {
+  // console.log(details);
+  alert("Thank You");
+  grade();
+} else if (details == "no" || "n") {
+  //  console.log(details);
+  alert("Not Problem");
 }
 
+let people = prompt("Are you working or is still student(YES OR NO)?");
 
-let view=prompt("Do you have a desire to share my webpage in your webpages? ")
-switch(people.toLowerCase()) {
-    case 'yes':
-        case 'y':
-            // console.log("people")
-            alert("Thankyou for your effort")
-            break;
+switch (people.toUpperCase()) {
+  case "YES":
+  case "Y":
+    alert("GOOD LUCK");
+    // console.log(people);
+    grade();
+    break;
 
-            case 'no':
-                case 'n':
-                    // console.log("people")
-                    alert("Try to think about your decision")
-                    break;
+  case "NO":
+  case "N":
+    alert("best wishes for you");
+    // console.log(people);
+    break;
+
+    defult: alert("wrong answer");
 }
 
-let h=prompt("What is your rate about this page?")
+let view = prompt(
+  "Do you have a desire to share my webpage in your webpages? "
+);
+switch (people.toLowerCase()) {
+  case "yes":
+  case "y":
+    // console.log("people")
+    alert("Thankyou for your effort");
+    grade();
+    break;
 
-for(let i=0 ;i<=3 ; i++ ) {
-        if(h==50){
-            alert (" you got the correct answer ")
-            break;
-        }
-         else if (h>50){
-            alert ("too high")
-            h=prompt("What is your rate about this page?");
-        }else if (h<50) {
-            alert ("too low")
-            h=prompt("What is your rate about this page?");
-        }
-if(i==3){
-    alert('the correct answer is 50')
-}
-    }
-
-
-
-
-    let remo=['Introduction','My Goals!','top ten places', 'facebook', 'twitter']
-    let mark = prompt("What most of thing you like it in my webpage" + remo + " ?")
-
- for(let rt=o ; rt<=5 ; i++ ){
- if (mark == "Introduction" || mark == "My Goals" || mark == "top ten places"){
-     mark=prompt("correct answer" , remo)
-    alert("It's Nice")
-     break;
- }
- else if (mark == "facebook" || mark == "twitter"){
-     alert("it's wrong")
-     mark=prompt("What most of thing you like it in my webpage ?")
- }
-
-
- if (rt==5){
-     alert('Your attempts are over');
-     
- }
+  case "no":
+  case "n":
+    // console.log("people")
+    alert("Try to think about your decision");
+    break;
 }
 
-// let grade=prompt("")
+let h = prompt("What is your rate about this page?");
+
+for (let i = 0; i <= 3; i++) {
+  if (h == 50) {
+    alert(" you got the correct answer ");
+    grade();
+    break;
+  } else if (h > 50) {
+    alert("too high");
+    h = prompt("What is your rate about this page?");
+  } else if (h < 50) {
+    alert("too low");
+    h = prompt("What is your rate about this page?");
+  }
+  if (i == 3) {
+    alert("the correct answer is 50");
+  }
+}
+
+let remo = ["INTRODUCTION", "MY GOALS!", "TOP TEN PLACES"];
+
+let mark = prompt("What most of thing you like it in my webpage").toUpperCase();
+
+for (let attempt = 1; attempt < 6; attempt++) {
+  if (mark == remo[0] || mark == remo[1] || mark == remo[2]) {
+    alert("It's Nice");
+    grade();
+    break;
+  } else {
+    alert("Wrong");
+    mark = prompt("What most of thing you like it in my webpage").toUpperCase();
+  }
+}
+
+alert("Your score is " + score + " out of 7");
